7faf4aa093849233370088ed79b3db0192dc0750
jay-bulk <jay.bulk@tuta.io>
2023-03-23 23:35:19 -0600

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use crossterm::event::{Event, KeyCOde, KeyEvent};
-use crossterm::terminal;
+use crossterm::{event, terminal};
 use std::time::Duration;
 
 use std::io;
 use std::io::Read;
 
 struct CleanUp;
+struct Reader;
+struct Editor {
+    reader: Reader,
+}
 
 
 impl Drop for CleanUp {
@@ -14,32 +18,62 @@ impl Drop for CleanUp {
     }
 }
 
-fn main() {
-    let _cleanu_up = CleanuUp;
-    terminal::enable_raw_mode().expect("Could not turn on Raw mode");
-    loop {
-        if let Event::Key(event) = event::read().expect("Failed to read line") {
-            match event {
-                KeyEvent {
-                    code: KeyCode::Char('q),
-                    modifiers: event::KeyModifiers::NONE,
-                } => break,
-                _ => {
+
+impl Reader {
+    fn read_key(&self) -> crossterm::Result<KeyEvent> {
+        loop {
+            if event::poll(Duration::from_millis(500))? {
+                if let Event::Key(event) = event::read()? {
+                    return Ok(event);
                 }
             }
-            println!("{:?}\r", event);
-        };
+        }
+    }
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self { reader: Reader }
     }
 
-    let mut buf = [0; 1];
-    /*modify*/
-    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {
-        let character = buf[0] as char;
-        if character.is_control() {
-            println!("{}\r, character as u8)
-            } else {
-                println!("{}\r", character)
-                }
+    fn process_keypress(&self) -> crossterm:Result<bool> {
+        match self.reader.read_key()? {
+            KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: event::KeyModifiers::CONTROL,
+            } => return Ok(false),
+            _ => {}
         }
+        Ok(true)
+    }
+
+    fn run(&self) -> crossterm::Result<bool> {
+        self.process_keypress()
+    }
+}
+
+
+fn main() {
+    let _cleanu_up = CleanuUp;
+    terminal::enable_raw_mode()?;
+    loop {
+            if event::poll(Duration::from_millis(1000))? {
+            if let Event::Key(event) = event::read()? {
+                match event {
+                    KeyEvent {
+                        code: KeyCode::Char('q),
+                        modifiers: event::KeyModifiers::CONTROL,
+                    } => break,
+                    _ => {
+                        //todo
+                    }
+                }
+                println!("{:?}\r", event);
+            };
+        } else {
+            println!("No inpuet yet\r");
+            }
+    }
+    Ok(())
 }
 
