7f9a671055c29276c9d0a49ed1fdd11af1a5f144
jay-bulk <jay.bulk@tuta.io>
2023-03-23 23:24:13 -0600

    adding crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use crossterm::event::{Event, KeyCOde, KeyEvent};
 use crossterm::terminal;
+use std::time::Duration;
+
 use std::io;
 use std::io::Read;
 
@@ -12,11 +15,31 @@ impl Drop for CleanUp {
 }
 
 fn main() {
+    let _cleanu_up = CleanuUp;
     terminal::enable_raw_mode().expect("Could not turn on Raw mode");
+    loop {
+        if let Event::Key(event) = event::read().expect("Failed to read line") {
+            match event {
+                KeyEvent {
+                    code: KeyCode::Char('q),
+                    modifiers: event::KeyModifiers::NONE,
+                } => break,
+                _ => {
+                }
+            }
+            println!("{:?}\r", event);
+        };
+    }
+
     let mut buf = [0; 1];
     /*modify*/
-    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
-    /*end*/
-    painc!();
-    //terminal::disable_raw_mode().expect("Could not turn off raw mode");
+    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {
+        let character = buf[0] as char;
+        if character.is_control() {
+            println!("{}\r, character as u8)
+            } else {
+                println!("{}\r", character)
+                }
+        }
 }
+
